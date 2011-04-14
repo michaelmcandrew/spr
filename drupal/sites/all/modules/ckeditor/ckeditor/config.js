f373ddd4f11c53c49b361023b43e1322f8757e39
Piyush Shah <piyush@pecansolutions.com>
2011-04-14 23:03:58 +0100

    changes to allow CiviCRM to use Drupal ckeditor

--- drupal/sites/all/modules/ckeditor/ckeditor/config.js
+++ drupal/sites/all/modules/ckeditor/ckeditor/config.js
@@ -8,4 +8,22 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+  /* DrupalFiltered toolbar below modified to be similar to earlier FCKeditor version */
+  config.toolbar_DrupalFiltered = [
+    ['Source'],
+    ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+    ['Undo','Redo','Find','Replace','-','SelectAll','RemoveFormat'],
+    ['Link','Unlink','Anchor','LinkToNode', 'LinkToMenu'],
+    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar'],
+    '/',
+    ['Format'],
+    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+    ['DrupalBreak', 'DrupalPageBreak'],
+    ['Maximize', 'ShowBlocks']
+   ];
+   config.toolbar = 'DrupalFiltered';
+   //config.width = '100%';
 };
