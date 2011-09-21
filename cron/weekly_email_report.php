91ea985ad4cafe23b16decce3a08c551bb9c9c83
Peter McAndrew <petermcandrew@thirdsectordesign.org>
2011-09-21 17:49:53 +0100

    Adding Weekly email report script for cronjob

---
+++ cron/weekly_email_report.php
@@ -0,0 +1,3 @@
+<?php
+fopen("http://www.spr.ac.uk/main/sites/all/modules/civicrm/bin/CiviReportMail.php?instanceId=31&reset=1&sendmail=1&name=CronUser&pass=c70nU53r@u!o&key=XJpoUMRC0okBeodx0JF3EGd9bJux5oCZ", "r");
+?>
